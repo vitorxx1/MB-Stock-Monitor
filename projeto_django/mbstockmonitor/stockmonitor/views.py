bbd342f6194e3910bd853d6e8d563bc1429a358a
Vítor Ângelo <vitor_angelo1@hotmail.com>
2021-07-25 10:17:33 -0300

    Atualizando Proj Django
    Inserindo a função de atualizar os dados históricos do banco

--- projeto_django/mbstockmonitor/stockmonitor/views.py
+++ projeto_django/mbstockmonitor/stockmonitor/views.py
@@ -3,14 +3,21 @@ from django.http import HttpResponse
 from django.db import connection
 import json
 import yfinance as yf
-from datetime import date
+from datetime import date, timedelta
 # Create your views here.
 
 def home(request):
 
-	dale = ['PETR4']
+	ticker = 'PETR4'
+	last_day = last_day_bd(ticker)
+
+	if last_day == date.today():
+		return HttpResponse('Chama no bresque')
+	else:
+		get_hist_data(last_day + timedelta(1), ticker)
+
 	with connection.cursor() as cursor:
-		cursor.execute("select * from cotacao where aco_codigo = %s",dale)
+		cursor.execute("select * from cotacao where aco_codigo = %s",[ticker])
 		query = dict_return(cursor)
 
 	dados_historicos = {}
@@ -41,10 +48,47 @@ def home(request):
 
 	return HttpResponse(json.dumps(acoes))
 
-
-
 def dict_return(cursor):
-    "Return all rows from a cursor as a dict"
+    #Return all rows from a cursor as a dict
     columns = [col[0] for col in cursor.description]
     
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+def last_day_bd(ticker):
+
+	with connection.cursor() as cursor:
+		cursor.execute('select max(cot_data) from cotacao where aco_codigo = %s',[ticker])
+		data = cursor.fetchone()
+
+	return data[0];
+
+def get_hist_data(start, ticker):
+
+	try:
+		   df = yf.download('{}.SA'.format(ticker),start=str(start))
+	except:
+		print(f'Falhou para {ticker}')
+		    
+	#removendo coluna Adj Close e removendo linhas com Na values
+	df = df.drop(columns=['Adj Close'])
+	df = df.dropna()
+	#tratando possíveis valores errados de cotação
+	columns = list(df.columns)
+	columns.pop()
+	for column in columns:
+		   df.loc[df[column] > 9999, column] = df.loc[df[column] > 9999, column]/1000
+	#orient indica quais são os valores de orientação do dicionário
+	#nesse caso, as chaves do dicionário são os valores dos índices dos elementos do DF
+	dict_acao = df.to_dict(orient='index')
+		        
+	with connection.cursor() as cursor:
+		for key in dict_acao:
+			if (key.date() >= start):
+				cursor.execute("insert into cotacao values (%s,%s,%s,%s,%s,%s,%s)",[key.date(),ticker,
+																				          dict_acao[key]['Open'],
+																				          dict_acao[key]['High'],
+																				          dict_acao[key]['Low'],
+																				          dict_acao[key]['Close'],
+																				          dict_acao[key]['Volume']])
+		        
+		        
